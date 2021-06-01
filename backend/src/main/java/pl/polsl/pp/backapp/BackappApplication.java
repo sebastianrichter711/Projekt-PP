57490701b27912428ce07cf54e8fb20632218943
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2021-06-01 15:37:15 +0200

    Add swagger

--- backend/src/main/java/pl/polsl/pp/backapp/BackappApplication.java
+++ backend/src/main/java/pl/polsl/pp/backapp/BackappApplication.java
@@ -2,12 +2,41 @@ package pl.polsl.pp.backapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackappApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("REST API Document")
+                .description("work in progress")
+                .termsOfServiceUrl("localhost")
+                .version("1.0")
+                .build();
+    }
 }
