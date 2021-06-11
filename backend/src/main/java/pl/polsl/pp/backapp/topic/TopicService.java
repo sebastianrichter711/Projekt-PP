8d7951bec6a44439f71c452f04afd8eae6f90ce5
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2021-06-11 12:47:54 +0200

    Make new topic's author currently logged user, fix update and delete topics

--- backend/src/main/java/pl/polsl/pp/backapp/topic/TopicService.java
+++ backend/src/main/java/pl/polsl/pp/backapp/topic/TopicService.java
@@ -1,21 +1,26 @@
 package pl.polsl.pp.backapp.topic;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import pl.polsl.pp.backapp.auth.UserPrincipal;
 import pl.polsl.pp.backapp.exception.IdNotFoundInDatabaseException;
+import pl.polsl.pp.backapp.user.User;
+import pl.polsl.pp.backapp.user.UserRepository;
 
 import java.util.Date;
-import java.util.Optional;
 
 
 @Service
 public class TopicService {
 
     private TopicRepository topicRepository;
+    private UserRepository userRepository;
 
-    public TopicService(TopicRepository topicRepository) {
+    public TopicService(TopicRepository topicRepository, UserRepository userRepository) {
         this.topicRepository = topicRepository;
+        this.userRepository = userRepository;
     }
 
     public Iterable<Topic> getTopics() {
@@ -30,22 +35,37 @@ public class TopicService {
     }
 
     public Topic addTopic(Topic topic) {
+        UserPrincipal userPrincipal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userRepository.findByLogin(userPrincipal.getUsername());
+
         topic.setCreateDate(new Date());
+        topic.setLastChange(new Date());
+        topic.setAuthor(user);
+
+        user.incPostsNumber();
+        userRepository.save(user);
         return topicRepository.save(topic);
     }
 
     public Topic updateTopic(String id, Topic updatedTopic) {
-        Optional<Topic> topic = topicRepository.findById(id);
+        Topic topic = topicRepository.findById(id)
+                .orElseThrow(() -> new IdNotFoundInDatabaseException("Topic of id " + id + " not found"));
 
-        if(topic.isEmpty())
-            throw new IdNotFoundInDatabaseException("Topic of id " + id + " not found");
+        topic.setLastChange(new Date());
+        topic.setDescription(updatedTopic.getDescription());
+        topic.setTitle(updatedTopic.getTitle());
 
-        updatedTopic.setLastChange(new Date());
         return topicRepository.save(updatedTopic);
     }
 
     public void deleteTopic(String id) {
         try {
+            UserPrincipal userPrincipal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = userRepository.findByLogin(userPrincipal.getUsername());
+
+            user.decPostsNumber();
+            userRepository.save(user);
+
             topicRepository.deleteById(id);
         } catch (IdNotFoundInDatabaseException e) {
             System.out.println(e.getMessage());
